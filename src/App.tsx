9a0a59a2a6b1914ce3cbb46769fff0edea6f2dd4
Aleksey <115175632+B2ndlaw@users.noreply.github.com>
2024-06-26 21:17:50 +0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
-
 import "./App.css";
-import { Counter } from "./components/Counter";
+import { Counter } from "./components/pages/Counter";
+import { Controller } from "./components/pages/Controller";
+import styled from "styled-components";
 
 export function App() {
-  return(
-    <Counter/>
-  )
+  return (
+    <FlexWrapper>
+      <Controller />
+      <Counter />
+    </FlexWrapper>
+  );
 }
+
+const FlexWrapper = styled.section`
+background: url("https://img.goodfon.com/original/1680x1050/b/6a/fallout-vault-boy-bethesda.jpg") no-repeat center / cover;
+height: 100vh;
+width: 100%;
+display: flex;
+align-items: center;
+`
+
+
